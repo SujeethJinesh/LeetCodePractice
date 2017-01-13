4db26201d8118ff4a41e56b153fe2f11ff78b7ab
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2017-01-13 14:46:17 -0500

    added the Max Subarray problem

---
+++ src/MaxSubarray.java
@@ -0,0 +1,49 @@
+/**
+ * Created by admin on 1/13/17.
+ */
+public class MaxSubarray {
+
+    public static void main(String[] args) {
+        int a[] = {-2, -3, -4, -1, -2, -1, -5, -3};
+
+        System.out.println(maxSubarray(a));
+    }
+
+
+    /**
+     * I will be using Kadane's algorithm which is a single pass O(n) algo.
+     *
+     * Time Complexity: O(n)
+     *
+     * Space Complexty: O(1) -- no additional space used other than entered array
+     *
+     * @param arr
+     * @return
+     */
+    public static int maxSubarray(int[] arr) {
+        if (arr.length == 1) {
+            return arr[0];
+        }
+        int runningSum = 0;
+        int currentMax = Integer.MIN_VALUE;
+
+        //need to run through each array
+
+        for (int i : arr) {
+
+            runningSum += i;
+
+            if (runningSum > currentMax) {
+                currentMax = runningSum;
+            }
+
+            if (runningSum < 0) {
+                runningSum = 0;
+            }
+
+        }
+
+        return currentMax;
+    }
+
+}
